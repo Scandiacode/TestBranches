209a0cdfa45bdfd1861bfcdda90498270d8cf0af
scott <scottjthayer@gmail.com>
2021-10-07 13:16:39 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitPracticeMergingBranches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
