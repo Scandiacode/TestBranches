983420a858b6553f2bb31a3d544ee77f0d377103
scott <scottjthayer@gmail.com>
2021-10-07 13:26:08 -0400

    second commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace GitPracticeMergingBranches
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Method1(3, 5));
+        }
+
+        static int Method1 (int num1, int num2)
+        {
+            if ( num1 > num2)
+            {
+                return num1;
+            }
+            return num2;
+        }
+        static int Method2 (int num1, int num2)
+        {
+            
         }
     }
 }
